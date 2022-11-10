19e4839511afd8ec5fc6f783b58668b3a3afa57f
Raja Bilal Nazir <rajabilalnazir@gmail.com>
2022-11-10 15:50:15 +0000

    Getting data using function

--- Visualization.py
+++ Visualization.py
@@ -13,14 +13,29 @@ import io
 import requests
 
 
-url="https://www.metoffice.gov.uk/pub/data/weather/uk/climate/datasets/Sunshine/date/UK.txt"
-s=requests.get(url).content
-df=pd.read_csv(io.StringIO(s.decode('utf-8')), delimiter='\s+', skiprows=(5), index_col=False)
+def getDataSet():
+    '''
+    This function retruns data after fetching data from web and read using pandas and save it in a dataframe.
+    Selects specific rows data and validate data types.
+    '''
+    url = "https://www.metoffice.gov.uk/pub/data/weather/uk/climate/datasets/Sunshine/date/UK.txt"
+    s = requests.get(url).content
+    df = pd.read_csv(io.StringIO(s.decode('utf-8')), delimiter='\s+', skiprows=(5), index_col=False)
+    
+    #Drop N/A data to clean data
+    df.dropna()
+    # Selecting specific years data for clean visualization
+    df = df.loc[30:102]
+    # Printing dataypes of dataframe to validate data.
+    df.dtypes
+    #Converting datatype from object to float64 so we can work with numerical data.
+    df['win'] = df['win'].astype(dtype='float64')
+    
+    return df
 
-df.dropna() #Drop N/A data to clean data
-df = df.loc[30:102] # Selecting specific years data for clean visualization
 
-df.dtypes # checking data types of all columns in dataframe. If all columns are valid to work with
-df['win'] = df['win'].astype(dtype='float64') #Converting datatype from Object to float64 so we can work with numerical data
+df = getDataSet()
+
+
 
 
