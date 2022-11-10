6876fcb92e8a2328577bba562865aa36dfd9a008
Raja Bilal Nazir <rajabilalnazir@gmail.com>
2022-11-10 17:55:46 +0000

    Final Update Visualization.py
    Doc string convention -- PEP 257
    Add Pie Chart

--- Visualization.py
+++ Visualization.py
@@ -14,8 +14,7 @@ import requests
 
 
 def getDataSet():
-    '''
-    This function retruns dataframe after fetching data from web and read using pandas and save it in a dataframe.
+    '''This function retruns dataframe after fetching data from web and read using pandas and save it in a dataframe.
     Selects specific rows data and validate data types.
     '''
     url = "https://www.metoffice.gov.uk/pub/data/weather/uk/climate/datasets/Sunshine/date/UK.txt"
@@ -34,11 +33,10 @@ def getDataSet():
     return df
 
 def plotLineChart():
-    '''
-    This function plot a line chart using dataframe.
+    
+    '''This function plot a line chart using dataframe.
     Line plot represents graphical visual of sunlight duration in UK bewteen 1947 - 2021.
     '''
-    
     
     fig, axs = plt.subplots(2, 2, figsize=(8, 4))
     # Title for line plot
@@ -73,8 +71,7 @@ def plotLineChart():
     fig.tight_layout()
     
 def plotBarChart():
-    '''
-    This function plot bar chart from the dataset using dataframe.
+    '''This function plot bar chart from the dataset using dataframe.
     Bar chart shows duration of sunlight in UK between 2000-2005 for all seasons.
     '''
     # loc function used to get data of specific years 2000-2005 from dataframe.
@@ -102,11 +99,58 @@ def plotBarChart():
     # legend function is called to make labels visibles on the chart like Seanson names.
     plt.legend()
     plt.show()
+    
+
+def getDataForPieChart(df_pie):
+    '''This function returns calculated data for pie charts using dataframe.
+    
+    Keyword arguments:
+    df_pie -- dataframe required of specific years of the same dataset used in this file. 
+    
+    Returns
+    -------
+    numpy array of selected data for pie chart
+    '''
+    #Sum of all years data season wise and calculate thier percentage to show data on plots
+    _win = np.sum(df_pie['win']) / np.sum(df_pie['ann'])*100
+    _spr = np.sum(df_pie['spr']) / np.sum(df_pie['ann'])*100
+    _sum = np.sum(df_pie['sum']) / np.sum(df_pie['ann'])*100
+    _aut = np.sum(df_pie['aut']) / np.sum(df_pie['ann'])*100
+    pie_array = np.array([_win, _sum, _spr, _aut])
+    return pie_array
+    
+
+def plotPieChart():
+    '''This function plots 2 pie charts presenting data from data sets.
+    The first pie charts shows the comparison of sunlight duartion between all seasons of years 1951-1955.
+    The second pie charts shows the comparison of sunlight duartion between all seasons of years 2015-2020.
+    '''
+    
+    # loc function used to get data of years between 2015-2020.
+    df_pie1 = df.loc[32:36]
+    df_pie2 = df.loc[97:101]
+    
+    pie1 = getDataForPieChart(df_pie1)
+    pie2 = getDataForPieChart(df_pie2)
+    
+    seasons = ['Winter', 'Summer', 'Spring', 'Autmn']
+    explode = (0, 0, 0.1, 0)
+
+    fig = plt.figure(figsize=(8, 4), dpi=144)
+
+    ax1 = fig.add_subplot(121)
+    ax1.pie(pie1, labels=seasons, autopct='%1.1f%%', shadow=True, explode=explode)
+    ax1.set_title('Sunlight duration in UK (1951-1955)')
+
+    ax2 = fig.add_subplot(122)
+    ax2.pie(pie2, labels=seasons, autopct='%1.1f%%', shadow=True, explode=explode)
+    ax2.set_title('Sunlight duration in UK (2015-2020)')
+
 
 
 df = getDataSet()
 plotLineChart()
 plotBarChart()
-
+plotPieChart()
 
 
